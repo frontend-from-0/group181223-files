f53445eeabe6d9b7031c37c1563b3a9300857d27
Anna S. A <anna.atalay@minna.tech>
2024-05-20 21:29:27 +0200

    Add Lesson19 completed

--- Lesson19/script.js
+++ Lesson19/script.js
@@ -1,4 +1,6 @@
-//Example
+// Recursion
+
+// Example
 // function doSomething(parameter) {
 //   // base condition for recursive function
 //   if (condition) {
@@ -30,25 +32,90 @@ getFactorial(5)
   5 * 4 * 3 * getFactorial(2)
   5 * 4 * 3 * 2 * getFactorial(1)
   5 * 4 * 3 * 2 * 1 * getFactorial(0)
-  5 * 4 * 3 * 2 * 1 * 1
-  
+  5 * 4 * 3 * 2 * 1 * 1 
 */
 console.log('5', getFactorial(5));
 console.log('-26', getFactorial(-26));
 console.log('String', getFactorial('sadasfasd'));
-
+console.log('--------------');
 // 1. Write a recursive function to print out all the elements of an array.
-const arr = [1, 2, 3, 4, 5];
+const arr = [1, 2, 30, 4, 5];
+function printArrayElements(arr, i = 0) {
+	if (i === arr.length) {
+		return;
+	} else {
+		console.log(arr[i]);
+		printArrayElements(arr, i + 1);
+	}
+}
+printArrayElements(arr);
 
-
+console.log('--------------');
 // 2. Write a recursive function to capitalize the first letter of each word in a sentence.
 const ex2sentence =
 	'a recursive function to capitalize the first letter of each word in a sentence.  ';
+function capitalizeLetter(sentence) {
+	if (!sentence.trim().length) {
+		// if length of the sentence is not a positive number
+		return '';
+	} else {
+		const words = sentence.split(' ');
+		const firstWord = words[0];
+		const firstWordCapitalized =
+			firstWord[0].toUpperCase() + firstWord.slice(1);
 
+		return (
+			firstWordCapitalized + ' ' + capitalizeLetter(words.slice(1).join(' '))
+		);
+	}
+}
+
+console.log(capitalizeLetter(ex2sentence));
+
+// if ('sdasd')
+// `if (!!'sasad')` is the same as `if (Boolean('sasad'))`
+/*
+1. Define function
+2. Define base condition: if there no more words left in the sentence, exit
+3. Split string into array of words
+4. Take first element of array, capitalize the first letter
+5. Call the function recursivly with the same sentence without the first word
+
+*/
 
 // 3. Write a recursive function to count the number of vowels in a string.
 const sentenceEx4 = 'Hello, how are you?';
 const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+function countVowels(string, index = 0, count = 0) {
+	if (index === string.length) {
+    return count;
+	}
+
+  if (vowels.includes(string[index])) {
+    count = count + 1;
+  }
+
+  // The issue was the missing 'return' keyword in the line below. We need to use return keyword in both base condition and here if we want the function to return the result (unlike ex. 1 where the result was only printed to terminal). If we omit return in the recursive call, the function will still run, but the accumulated count would not be returned to the initial call, resulting in undefined.
+  return countVowels(string, index + 1, count);
+}
+
+console.log(countVowels(sentenceEx4));
+
+
+
+function countVowelsLoop(string) {
+  let count = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (vowels.includes(string[i])) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+console.log(countVowelsLoop(sentenceEx4));
 
 // 4. Write a recursive function to remove all occurrences of a specified character from a string.
 const sentenceEx5 = 'Hello, how are you?';
