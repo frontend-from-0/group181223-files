b875a0e0d5a118da1966b30525c3c3b792364b62
Anna S. A <anna.atalay@minna.tech>
2024-05-27 19:23:46 +0200

    Add lesson starter files

---
+++ Lesson20/oop.js
@@ -0,0 +1,11 @@
+  /* Programming paradigms
+  OOP - Object Oriented programming
+  FP - Functional programming
+
+  OOP:  state is usually stored in objects and can be modified through methods 
+  FP: state is immutable, and functions are designed to transform data rather than mutate it
+  
+  OOP: classes encapsulates related data and behavior
+  FP: problems are broken down into smaller, composable functions that can be combined to solve larger problems.
+
+*/
