2a5cbf01bcd07665fc3cd448e6271b5afa9151fb
Anna S. A <anna.atalay@minna.tech>
2024-09-02 15:48:46 +0200

    Add Lesson31 starter files

---
+++ Lesson31/demo-app/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+
+// Name your React component with capitalised first letter
+export function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+    );
+}
+
